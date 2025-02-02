656d7bfe00cd82a159720e64f01420a2c623ea2d
Tom <70593510+MrAlexanderk@users.noreply.github.com>
2025-02-02 17:54:40 -0300

    navbar fixed;

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,24 @@
 
 const Navbar = () => {
-    const login = document.getElementById('logged-in')
-    const logout = document.getElementById('logged-out')
-
     const token = true
     const total = 25000
 
 
-
-
     return (
       <>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Mamma Mia!</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Mamma Mia!</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav gap-3">
-                        <a class="nav-link active option" aria-current="page" href="#">🍕Home</a>
-                        <div id="logged-in" className="d-flex gap-2">
-                            <a class="nav-link option" href="#">🔓Profile</a>
-                            <a class="nav-link option" href="#">🔒Logout</a>
-                        </div>
-                        <div id="logged-out" className="d-flex gap-2">
-                            <a class="nav-link option" href="#">🔐Login</a>
-                            <a class="nav-link option" href="#">🔐Register</a>
-                        </div>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav gap-3 ms-auto">
+                        <a className="nav-link active option" aria-current="page" href="#">🍕Home</a>
+                        <a className={`nav-link option ${!token ? 'd-none' : ''}`} href="#">🔓Profile</a>
+                        <a className={`nav-link option ${!token ? 'd-none' : ''}`} href="#">🔒Logout</a>
+                        <a className={`nav-link option ${token ? 'd-none' : ''}`} href="#">🔐Login</a>
+                        <a className={`nav-link option ${token ? 'd-none' : ''}`} href="#">🔐Register</a>
                         <a class="nav-link option" href="#">🛒Total: {total}</a>
                     </div>
                 </div>
