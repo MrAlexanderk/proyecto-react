1533bcf7e531c292687bc9664b16946464937500
Tom <70593510+MrAlexanderk@users.noreply.github.com>
2025-02-02 17:45:35 -0300

    navbar items added;

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 
 const Navbar = () => {
+    const login = document.getElementById('logged-in')
+    const logout = document.getElementById('logged-out')
+
+    const token = true
+    const total = 25000
+
+
+
+
     return (
       <>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -9,11 +18,17 @@ const Navbar = () => {
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Pricing</a>
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <div class="navbar-nav gap-3">
+                        <a class="nav-link active option" aria-current="page" href="#">🍕Home</a>
+                        <div id="logged-in" className="d-flex gap-2">
+                            <a class="nav-link option" href="#">🔓Profile</a>
+                            <a class="nav-link option" href="#">🔒Logout</a>
+                        </div>
+                        <div id="logged-out" className="d-flex gap-2">
+                            <a class="nav-link option" href="#">🔐Login</a>
+                            <a class="nav-link option" href="#">🔐Register</a>
+                        </div>
+                        <a class="nav-link option" href="#">🛒Total: {total}</a>
                     </div>
                 </div>
             </div>
